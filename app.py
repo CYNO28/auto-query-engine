460f195a72accd66fc8a94df27f9ca253006024d
Dinesh Singh Bisht <44347226+CYNO28@users.noreply.github.com>
2023-10-10 15:03:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ apiKey1 :str =os.getenv("PINECONE_API_KEY")
 
 
 pinecone.init(api_key=apiKey1, environment="gcp-starter")
-pinecone_index = pinecone.Index("test")
+pinecone_index = pinecone.Index("hackathon")
 
 os.environ["OPENAI_API_KEY"] = apiKey
 
@@ -41,7 +41,7 @@ from llama_index.llms import OpenAI
 
 
 chunk_size = 1024
-llm = OpenAI(temperature=0, model="gpt-4", streaming=True)
+llm = OpenAI(temperature=0, streaming=True)
 service_context = ServiceContext.from_defaults(chunk_size=chunk_size, llm=llm)
 text_splitter = TokenTextSplitter(chunk_size=chunk_size)
 node_parser = SimpleNodeParser.from_defaults(text_splitter=text_splitter)
@@ -49,7 +49,7 @@ node_parser = SimpleNodeParser.from_defaults(text_splitter=text_splitter)
 
 #  vector store
 vector_store = PineconeVectorStore(
-    pinecone_index=pinecone_index, namespace="wiki_cities"
+    pinecone_index=pinecone_index
 )
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 vector_index = VectorStoreIndex([], storage_context=storage_context)
@@ -74,66 +74,64 @@ from llama_index.indices.struct_store.sql_query import NLSQLTableQueryEngine
 
 sql_query_engine = NLSQLTableQueryEngine(sql_database,tables=["city_stats"],)
 
-res=sql_query_engine.query("what is the country for tokyo")
 
-print(str(res))
+#---------------------------------------no need---------------------------------
+
+# cars = ["Tokyo"]
+# wiki_docs = WikipediaReader().load_data(pages=cars, auto_suggest=False)
+
+# # print('done fetch wiki_docs')
 
 
-# cities = ["Toronto", "Berlin", "Tokyo"]
-# wiki_docs = WikipediaReader().load_data(pages=cities)
-
-# print('done fetch wiki_docs')
-
-# for city, wiki_doc in zip(cities, wiki_docs):
+# for city, wiki_doc in zip(cars, wiki_docs):
 #     nodes = node_parser.get_nodes_from_documents([wiki_doc])
-    
+
 #     for node in nodes:
 #         node.metadata = {"title": city}
-#     vector_store.insert_nodes(nodes)
-# print('done till here')
+#     vector_index.insert_nodes(nodes)
+
+#---------------------------------------no need---------------------------------
+
+from llama_index.query_engine import SQLAutoVectorQueryEngine, RetrieverQueryEngine
+from llama_index.tools.query_engine import QueryEngineTool
+from llama_index.indices.vector_store import VectorIndexAutoRetriever
+from llama_index.indices.vector_store.retrievers import VectorIndexAutoRetriever
+from llama_index.vector_stores.types import MetadataInfo, VectorStoreInfo
+from llama_index.query_engine.retriever_query_engine import RetrieverQueryEngine
 
 
+vector_store_info = VectorStoreInfo(
+    content_info="articles about different cities",
+    metadata_info=[
+        MetadataInfo(name="title", type="str", description="The name of the city"),
+    ],
+)
+vector_auto_retriever = VectorIndexAutoRetriever(
+    vector_index, vector_store_info=vector_store_info
+)
 
-# from llama_index.query_engine import SQLAutoVectorQueryEngine, RetrieverQueryEngine
-# from llama_index.tools.query_engine import QueryEngineTool
-# from llama_index.indices.vector_store import VectorIndexAutoRetriever
-# from llama_index.indices.vector_store.retrievers import VectorIndexAutoRetriever
-# from llama_index.vector_stores.types import MetadataInfo, VectorStoreInfo
-# from llama_index.query_engine.retriever_query_engine import RetrieverQueryEngine
-
-
-# vector_store_info = VectorStoreInfo(
-#     content_info="articles about different cities",
-#     metadata_info=[
-#         MetadataInfo(name="title", type="str", description="The name of the city"),
-#     ],
-# )
-# vector_auto_retriever = VectorIndexAutoRetriever(
-#     vector_index, vector_store_info=vector_store_info
-# )
-
-# retriever_query_engine = RetrieverQueryEngine.from_args(
-#     vector_auto_retriever, service_context=service_context
-# )
-# sql_tool = QueryEngineTool.from_defaults(
-#     query_engine=sql_query_engine,
-#     description=(
-#         "Useful for translating a natural language query into a SQL query over a table containing: "
-#         "city_stats, containing the population/country of each city"
-#     ),
-# )
-# vector_tool = QueryEngineTool.from_defaults(
-#     query_engine=retriever_query_engine,
-#     description=f"Useful for answering semantic questions about different cities",
-# )
+retriever_query_engine = RetrieverQueryEngine.from_args(
+    vector_auto_retriever, service_context=service_context
+)
+sql_tool = QueryEngineTool.from_defaults(
+    query_engine=sql_query_engine,
+    description=(
+        "only use this for getting population"
+        
+    ),
+)
+vector_tool = QueryEngineTool.from_defaults(
+    query_engine=retriever_query_engine,
+    description=f"use this for details of data",
+)
 
 
-# query_engine = SQLAutoVectorQueryEngine(
-#     sql_tool, vector_tool, service_context=service_context
-# )
+query_engine = SQLAutoVectorQueryEngine(
+    sql_tool, vector_tool, service_context=service_context
+)
 
 
-# response = query_engine.query(
-#     "Tell me about the arts and culture of the city with the highest population"
-# )
-# print(str(response))
+response = query_engine.query(
+    "tell me about Tokyo and its population and tell me more about Tokyo "
+)
+print(str(response))
